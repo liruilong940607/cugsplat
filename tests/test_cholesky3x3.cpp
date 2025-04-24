6440d7e0f1c08b7a7af181e4736298a91d4730bc
Ruilong Li <397653553@qq.com>
2025-04-24 17:53:21 +0000

    cleanup

--- tests/test_cholesky3x3.cpp
+++ tests/test_cholesky3x3.cpp
@@ -8,8 +8,6 @@
 #include "glm_tensor.h"
 #include "math/cholesky3x3.h"
 
-using namespace gsplat;
-
 void test_cholesky() {
     glm::fmat3 A(0.0f);
     A[0][0] = 4.0f;
@@ -19,7 +17,7 @@ void test_cholesky() {
     A[2][1] = A[1][2] = 1.0f;
     A[2][2] = 3.0f;
 
-    auto [L, ok] = cholesky(A);
+    auto [L, ok] = gsplat::cholesky(A);
     assert(ok && "cholesky decomposition failed");
 
     glm::fmat3 LLT = L * glm::transpose(L);
@@ -33,7 +31,7 @@ void test_forward_substitution() {
     L[2][2] = 3.0f;
     glm::fvec3 y(1.0f, 4.0f, 9.0f);
 
-    glm::fvec3 x = forward_substitution(L, y);
+    glm::fvec3 x = gsplat::forward_substitution(L, y);
     glm::fvec3 y_reconstructed = L * x;
 
     assert(glm::all(glm::equal(y, y_reconstructed, 1e-5f)));
@@ -46,7 +44,7 @@ void test_backward_substitution() {
     L[2][2] = 3.0f;
     glm::fvec3 y(3.0f, 2.0f, 1.0f);
 
-    glm::fvec3 x = backward_substitution(L, y);
+    glm::fvec3 x = gsplat::backward_substitution(L, y);
     glm::fvec3 y_reconstructed = glm::transpose(L) * x;
 
     assert(glm::all(glm::equal(y, y_reconstructed, 1e-5f)));
@@ -55,10 +53,10 @@ void test_backward_substitution() {
 void test_forward_substitution_vjp() {
     glm::fmat3 L = glm::fmat3(1.0f);
     glm::fvec3 y = glm::fvec3(1.0f);
-    glm::fvec3 x = forward_substitution(L, y);
+    glm::fvec3 x = gsplat::forward_substitution(L, y);
     glm::fvec3 v_x = glm::fvec3(1.0f);
 
-    glm::fmat3 v_L = forward_substitution_vjp(L, x, v_x);
+    glm::fmat3 v_L = gsplat::forward_substitution_vjp(L, x, v_x);
     assert(v_L[0][0] < 0);
 }
 
@@ -68,10 +66,10 @@ void test_cholesky_Winv_y() {
     A[1][0] = 2.0f; A[1][1] = 5.0f; A[1][2] = 1.0f;
     A[2][0] = 2.0f; A[2][1] = 1.0f; A[2][2] = 3.0f;
 
-    auto [L, ok] = cholesky(A);
+    auto [L, ok] = gsplat::cholesky(A);
     assert(ok && "cholesky decomposition failed");
     glm::fvec3 y(1.0f);
-    glm::fvec3 x = cholesky_Winv_y(L, y);
+    glm::fvec3 x = gsplat::cholesky_Winv_y(L, y);
     assert(glm::all(glm::equal(A * x, y, 1e-5f)));
 }
 
@@ -81,9 +79,9 @@ void test_cholesky_Winv() {
     A[1][0] = 2.0f; A[1][1] = 5.0f; A[1][2] = 1.0f;
     A[2][0] = 2.0f; A[2][1] = 1.0f; A[2][2] = 3.0f;
 
-    auto [L, ok] = cholesky(A);
+    auto [L, ok] = gsplat::cholesky(A);
     assert(ok && "cholesky decomposition failed");
-    glm::fmat3 Winv = cholesky_Winv(L);
+    glm::fmat3 Winv = gsplat::cholesky_Winv(L);
     assert(glm::all(glm::equal(A * Winv, glm::fmat3(1.0f), 1e-5f)));
 }
 
@@ -93,9 +91,9 @@ void test_cholesky_Linv() {
     A[1][0] = 2.0f; A[1][1] = 5.0f; A[1][2] = 1.0f;
     A[2][0] = 2.0f; A[2][1] = 1.0f; A[2][2] = 3.0f;
 
-    auto [L, ok] = cholesky(A);
+    auto [L, ok] = gsplat::cholesky(A);
     assert(ok && "cholesky decomposition failed");
-    glm::fmat3 Linv = cholesky_Linv(L);
+    glm::fmat3 Linv = gsplat::cholesky_Linv(L);
     assert(glm::all(glm::equal(L * Linv, glm::fmat3(1.0f), 1e-5f)));
 }
 
@@ -110,10 +108,10 @@ void test_cholesky_Linv_vjp() {
     v_Linv[0][1] = 0.3f; v_Linv[1][1] = -0.2f;
     v_Linv[0][2] = 0.5f; v_Linv[1][2] = 0.7f; v_Linv[2][2] = 2.0f;
 
-    auto [L, ok] = cholesky(A);
+    auto [L, ok] = gsplat::cholesky(A);
     assert(ok && "cholesky decomposition failed");
-    glm::fmat3 Linv = cholesky_Linv(L);
-    glm::fmat3 v_L = cholesky_Linv_vjp(L, v_Linv);
+    glm::fmat3 Linv = gsplat::cholesky_Linv(L);
+    glm::fmat3 v_L = gsplat::cholesky_Linv_vjp(L, v_Linv);
 
     // reference
     torch::Tensor L_torch = glm_to_tensor(L).requires_grad_(true);
@@ -141,9 +139,9 @@ void test_cholesky_vjp() {
     v_L[0][1] = 0.3f; v_L[1][1] = -0.2f;
     v_L[0][2] = 0.5f; v_L[1][2] = 0.7f; v_L[2][2] = 2.0f;
 
-    auto [L, ok] = cholesky(A);
+    auto [L, ok] = gsplat::cholesky(A);
     assert(ok && "cholesky decomposition failed");
-    glm::fmat3 v_A = cholesky_vjp(L, v_L);
+    glm::fmat3 v_A = gsplat::cholesky_vjp(L, v_L);
 
     // reference
     torch::Tensor A_torch = glm_to_tensor(A).requires_grad_(true);
