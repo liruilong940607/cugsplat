9ccba57af2ed3fe5beea3bd32c6310ab153a87b0
liruilong940607 <ruilongli94@gmail.com>
2025-05-01 20:06:27 -0700

    finish up shutter::point_world_to_image

--- gsplat/include/camera2/shutter.h
+++ gsplat/include/camera2/shutter.h
@@ -20,6 +20,7 @@ enum class Type {
     ROLLING_RIGHT_TO_LEFT,
 };
 
+// Compute the relative frame time for a given image point
 GSPLAT_HOST_DEVICE inline auto relative_frame_time(
     const glm::fvec2 &image_point,
     const std::array<uint32_t, 2> &resolution,
@@ -43,43 +44,81 @@ GSPLAT_HOST_DEVICE inline auto relative_frame_time(
     return t;
 }
 
+template <typename RotationType> struct PointWorldToImageResult {
+    glm::fvec2 image_point;
+    glm::fvec3 camera_point;
+    RotationType pose_r;
+    glm::fvec3 pose_t;
+    bool valid_flag = false;
+};
+
+// Project a world point to an image point using the start and end poses
+// and the shutter type
 template <size_t N_ITER = 10, typename RotationType, typename Func>
 GSPLAT_HOST_DEVICE inline auto point_world_to_image(
-    Func point_camera_to_image,
+    Func project_fn, // Function to project a camera point to an image point
     const glm::fvec3 &world_point,
     const RotationType &pose_r_start,
     const glm::fvec3 &pose_t_start,
     const RotationType &pose_r_end,
     const glm::fvec3 &pose_t_end,
     const Type &shutter_type
-) -> glm::fvec2 {
+) -> PointWorldToImageResult<RotationType> {
     static_assert(
         std::is_same_v<RotationType, glm::fmat3> ||
             std::is_same_v<RotationType, glm::fquat>,
         "RotationType must be either glm::fmat3 or glm::fquat"
     );
+
     // Always perform transformation using start pose
-    auto const camera_point =
+    auto const camera_point_start =
         se3::transform_point(pose_r_start, pose_t_start, world_point);
-    auto const image_point = point_camera_to_image(camera_point);
+    auto const &[image_point_start, valid_flag_start] =
+        project_fn(camera_point_start);
     if (shutter_type == Type::GLOBAL) {
-        return image_point;
+        return PointWorldToImageResult{};
+    }
+
+    // Initialize the image point using the start or end pose
+    glm::fvec2 init_image_point;
+    if (valid_flag_start) {
+        init_image_point = image_point_start;
+    } else {
+        auto const camera_point_end =
+            se3::transform_point(pose_r_end, pose_t_end, world_point);
+        auto const &[image_point_end, valid_flag_end] =
+            project_fn(camera_point_end);
+        if (valid_flag_end) {
+            init_image_point = image_point_end;
+        } else {
+            return PointWorldToImageResult{};
+        }
     }
 
     // Iterate to converge to the correct image point
-    auto image_point_rs = image_point;
+    auto image_point_rs = init_image_point;
+    glm::fvec3 camera_point_rs;
+    bool valid_flag_rs;
+    RotationType pose_r_rs;
+    glm::fvec3 pose_t_rs;
 #pragma unroll
     for (auto j = 0; j < N_ITER; ++j) {
         auto const t =
             relative_frame_time(image_point, resolution, shutter_type);
-        auto const &[pose_r_rs, pose_t_rs] = se3::interpolate(
+        std::tie(pose_r_rs, pose_t_rs) = se3::interpolate(
             t, pose_r_start, pose_t_start, pose_r_end, pose_t_end
         );
-        auto const camera_point_rs =
+        camera_point_rs =
             se3::transform_point(pose_r_rs, pose_t_rs, world_point);
-        image_point_rs = point_camera_to_image(camera_point_rs);
+        std::tie(image_point_rs, valid_flag_rs) = project_fn(camera_point_rs);
+        if (!valid_flag_rs) {
+            return PointWorldToImageResult{};
+        }
+        // TODO: add early exit and convergence check
     }
-    return image_point_rs;
+    return PointWorldToImageResult{
+        image_point_rs, camera_point_rs, pose_r_rs, pose_t_rs, true
+    };
 }
 
 } // namespace gsplat::shutter
