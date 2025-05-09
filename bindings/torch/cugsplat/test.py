5fe6e8330953dd6512beb84275910d6cea5de9cf
liruilong940607 <ruilongli94@gmail.com>
2025-05-09 00:47:59 -0700

    minor

--- bindings/torch/cugsplat/test.py
+++ bindings/torch/cugsplat/test.py
@@ -3,8 +3,8 @@ from _backend import _C
 
 device = "cpu"
 
-camera_points = torch.randn(1, 3, device=device)
-focal_lengths = torch.randn(1, 2, device=device)
-principal_points = torch.randn(1, 2, device=device)
+camera_points = torch.randn(1, 1, 3, device=device)
+focal_lengths = torch.randn(1, 1, 2, device=device)
+principal_points = torch.randn(1, 1, 2, device=device)
 image_points = _C.fisheye_project(camera_points, focal_lengths, principal_points)
 print (image_points)
